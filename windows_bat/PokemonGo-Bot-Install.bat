50796b0a9ffe632e7d218f5f3f4983b57ec63f56
Daniel Sterzek Dian <danielsdian@users.noreply.github.com>
2016-08-14 00:50:39 -0300

    Update PokemonGo-Bot-Install.bat
    Added backup routines to backup any existent config.json, userdata.js and encrypt files prior to removing the pokemongo-bot folder and installing it again.

--- windows_bat/PokemonGo-Bot-Install.bat
+++ windows_bat/PokemonGo-Bot-Install.bat
@@ -39,25 +39,35 @@ if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
 @ECHO Before proceeding, please install the following software:
 @ECHO. 
 @ECHO ---- Python 2.7.x
-@ECHO      http://docs.python-guide.org/en/latest/starting/installation/
+@ECHO      "http://docs.python-guide.org/en/latest/starting/installation/"
 @ECHO.
 @ECHO ---- git
-@ECHO      https://git-scm.com/book/en/v2/Getting-Started-Installing-Git
+@ECHO      "https://git-scm.com/book/en/v2/Getting-Started-Installing-Git"
 @ECHO.
 @ECHO ---- Protoc
-@ECHO      https://github.com/google/protobuf/releases/download/v3.0.0-beta-4/protoc-3.0.0-beta-4-win32.zip
+@ECHO      "https://github.com/google/protobuf/releases/download/v3.0.0-beta-4/protoc-3.0.0-beta-4-win32.zip"
 @ECHO.
 @ECHO ---- Microsoft Visual C++ Compiler for Python 2.7
-@ECHO      http://www.microsoft.com/en-us/download/details.aspx?id=44266
+@ECHO      "http://www.microsoft.com/en-us/download/details.aspx?id=44266"
 @ECHO.
 @ECHO ---- encrypt.so / encrypt.dll /encrypt_64.dll (Copy to the same folder as this batch file)
-@ECHO      Get them from our Slack chat! https://pokemongo-bot.herokuapp.com/
+@ECHO      Get them from our Slack chat! "https://pokemongo-bot.herokuapp.com/"
 @ECHO.
 @ECHO ---- If you already have a config.json and a userdata.js, copy to the same folder as this batch file.
 @ECHO.
 @ECHO.
 @ECHO.
 @PAUSE
+@ECHO.
+@ECHO.
+@ECHO.
+@ECHO --------------------Creating Backup--------------------
+@ECHO.
+@ECHO.
+@ECHO.
+COPY C:\Python27\PokemonGo-Bot\encrypt*.* "%~dp0\"
+COPY C:\Python27\PokemonGo-Bot\configs\config.json "%~dp0\"
+COPY C:\Python27\PokemonGo-Bot\web\config\userdata.js "%~dp0\"
 @ECHO.
 @ECHO.
 @ECHO.
@@ -72,7 +82,7 @@ pip2 install --upgrade pip
 pip2 install --upgrade virtualenv
 pip2 install --upgrade "%~dp0\PyYAML-3.11-cp27-cp27m-win32.whl"
 pip2 install --upgrade "%~dp0\PyYAML-3.11-cp27-cp27m-win_amd64.whl"
-git clone --recursive -b master https://github.com/PokemonGoF/PokemonGo-Bot
+git clone --recursive -b dev https://github.com/PokemonGoF/PokemonGo-Bot
 pip2 install --upgrade -r C:/Python27/PokemonGo-Bot/requirements.txt
 cd C:/Python27/PokemonGo-Bot/
 virtualenv .
